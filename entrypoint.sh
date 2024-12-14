c23202cedd0d3c5beb3ad114f204f43180a31760
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2024-12-14 02:59:10 +0000

    refactor entrypoint.sh to improve registry login handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,12 +18,21 @@ fi
 # Using the env variable REGAUTH is a list of new-line separated or '|||' separated registries where each line follows 
 # the format "registry,username,password" to login to the registry
 if [ -n "$REGAUTH" ]; then
-  echo "$REGAUTH" | tr '|||' '\n' | while IFS=, read -r registry username password; do
-    echo "Logins into $registry" >&2
-    echo -n "  docker: " >&2 && echo "$password" | docker login "$registry" --username "$username" --password-stdin 2>/dev/null
-    echo -n "  skopeo: " >&2 && echo "$password" | skopeo login --username "$username" --password-stdin "$registry" --tls-verify=false
-    echo -n "  helm  : " >&2 && echo "$password" | helm registry login "$registry" --username "$username" --password-stdin --insecure
-    echo "" >&2
+  OLDIFS="$IFS"
+  IFS='|||'
+  for entry in $REGAUTH; do
+    IFS=','
+    echo "$entry" | { 
+      read -r registry username password
+      if [ -n "$registry" ] && [ -n "$username" ] && [ -n "$password" ]; then
+        echo "Logging into $registry" >&2
+        echo -n "  docker: " >&2 && echo "$password" | docker login "$registry" --username "$username" --password-stdin 2>/dev/null
+        echo -n "  skopeo: " >&2 && echo "$password" | skopeo login --username "$username" --password-stdin "$registry" --tls-verify=false
+        echo -n "  helm  : " >&2 && echo "$password" | helm registry login "$registry" --username "$username" --password-stdin --insecure
+        echo "" >&2
+      fi
+    }
+    IFS="$OLDIFS"
   done
   unset REGAUTH
 fi
