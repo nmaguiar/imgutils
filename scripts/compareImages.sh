3d002446aa2d8009430d38b82ddbd98266377b12
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-02-24 23:12:48 +0000

    enhance compareImages.sh with directory existence checks and improved output formatting

--- scripts/compareImages.sh
+++ scripts/compareImages.sh
@@ -4,15 +4,32 @@
 # Version: 20250224
 #
 # Usage  :
-#   compareImages.sh <image1> <image2>
+#   compareImages.sh <image1> <image2> [output_format]
 #
 # Description :
 #   This script extracts the files from both docker image archive file to two directories to compare them
 
 TMPIMG=""
 
+# Check if directory A exists, if not create it
+if [ ! -e "A" ]; then
+    mkdir A
+else
+    echo "[31;1m⚠️  -- Directory A already exists. Please remove it before running this script (rm -rf A* B*).[m"
+    exit 1
+fi
+
+# Check if directory B exists, if not create it
+if [ ! -e "B" ]; then
+    mkdir B
+else
+    echo "[31;1m⚠️  -- Directory B already exists. Please remove it before running this script (rm -rf A* B*).[m"
+    exit 1
+fi
+
 # If $1 is not a file and contains a colon then use skopeo to copy the image to a temporary file.
-echo "-- Copying image $1 to a temporary file..."
+echo "[1m📡 -- Copying image $1 to a temporary file...[m"
+echo
 if [ ! -f "$1" ] && [[ "$1" == *:* ]]; then
     TMPIMG=$(mktemp)
     if [[ "$1" == docker-daemon:* ]]; then
@@ -25,22 +42,19 @@ else
     IMAGE="$1"
 fi
 
-# Check if directory A exists, if not create it
-if [ ! -e "A" ]; then
-    mkdir A
-else
-    echo "Directory A already exists. Please remove it before running this script."
-    exit 1
-fi
-
 # Extract the files from the image archive to directory A
-echo "-- Extracting files from image $1 to directory A..."
+echo
+echo "[1m💾 -- Extracting files from image $1 to directory A...[m"
+echo 
+
 /usr/local/bin/expandFilesInImageArchive.sh $IMAGE A
 
 TMPIMG=""
 
 # If $2 is not a file and contains a colon then use skopeo to copy the image to a temporary file.
-echo "-- Copying image $2 to a temporary file..."
+echo "[1m📡 -- Copying image $2 to a temporary file...[m"
+echo 
+
 if [ ! -f "$2" ] && [[ "$2" == *:* ]]; then
     TMPIMG=$(mktemp)
     if [[ "$2" == docker-daemon:* ]]; then
@@ -53,20 +67,18 @@ else
     IMAGE="$2"
 fi
 
-# Check if directory B exists, if not create it
-if [ ! -e "B" ]; then
-    mkdir B
-else
-    echo "Directory B already exists. Please remove it before running this script."
-    exit 1
-fi
-
 # Extract the files from the image archive to directory B
-echo "-- Extracting files from image $2 to directory B..."
+echo
+echo "[1m💾 -- Extracting files from image $2 to directory B...[m"
+echo
+
 /usr/local/bin/expandFilesInImageArchive.sh $IMAGE B
 
 # Comparing folders A and B
-echo "-- Comparing directories A and B..."
+echo
+echo "[1m🔎 -- Comparing directories A and B...[m"
+echo 
+
 DIR=A
 find "$DIR" -type f | while read -r file; do
     md5=$(md5sum "$file" | cut -f1 -d ' ')
@@ -83,5 +95,10 @@ find "$DIR" -type f | while read -r file; do
 done | sort > B.csv
 echo "file,size,lastModified,permissions,user,group,md5" | cat - B.csv > B.csv.tmp && mv B.csv.tmp B.csv
 
-echo "-- List of files changed, added or removed in B compared to A:"
-diff -u A.csv B.csv
+echo "[1m🗄️  -- Storing list of files changed, added or removed in B compared to A in AB-diff.csv[m"
+echo 
+oafp in=oafp data="[(file:'A.csv')|(file:'B.csv')]" set="(a:'[0]', b:'[1]')" setop=diffa out=csv > AB-diff.csv
+
+shift
+shift
+oafp AB-diff.csv in=csv out=ctable "$@"
